a2f879b944cd839f019ba75f140a416f0bc6e781
Ray Weitzenberg <ray@mechaneyes.com>
2021-04-06 08:10:55 -0400

    one target white to red to white to red
    currently each color on one target shows for 1s. going to randomize this and apply it to the others

--- Assets/Scripts/Targets_01.cs
+++ Assets/Scripts/Targets_01.cs
@@ -18,19 +18,19 @@ public class Targets_01 : MonoBehaviour
 
     public Transform[] targets;
 
-    public Vector3 singleDrop;
 
     void Start()
     {
         float step = 2f / resolution;
         var position = Vector3.zero;
         //var scale = Vector3.one * step;
-        var scale = Vector3.one * 0.09f;
+        var scale = Vector3.one * 0.07f;
 
         targets = new Transform[resolution];
         for (int i = 0; i < targets.Length; i++)
         {
             Transform oneTarget = Instantiate(targetPrefab);
+            oneTarget.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
             position.x = (i + 0.5f) * step - 1f;
             oneTarget.localPosition = position;
             oneTarget.localScale = scale;
@@ -38,6 +38,23 @@ public class Targets_01 : MonoBehaviour
             targets[i] = oneTarget;
 
             oneTarget.SetParent(transform, false);
+        }
+
+        StartCoroutine(Flicker());
+    }
+
+    IEnumerator Flicker()
+    {
+        while (true)
+        {
+            bool looper = true;
+            yield return new WaitForSeconds(1);
+            targets[4].GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+            looper = false;
+            looper = true;
+            yield return new WaitForSeconds(1);
+            targets[4].GetComponent<MeshRenderer>().material.SetColor("_Color", Color.white);
+            looper = false;
         }
     }
 
@@ -48,11 +65,17 @@ public class Targets_01 : MonoBehaviour
         {
             Transform oneTarget = targets[i];
             Vector3 position = oneTarget.localPosition;
+
+            //position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            // Dividing by 2 flattens the curve
             position.y = Mathf.Sin(Mathf.PI * (position.x + time) / 2);
             oneTarget.localPosition = position;
 
             //singleDrop = targets[i].position;
             //Debug.Log("singleDrop " + singleDrop);
         }
+
+        //InvokeRepeating("Flicker", 2.0f, 0.3f);
+        //StartCoroutine(Flicker());
     }
 }
